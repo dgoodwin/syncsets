07bcde1aa4cda3c8a50a9d68ff21c690bad2cd69
Devan Goodwin <dgoodwin@redhat.com>
2020-11-27 10:09:18 -0400

    Connect to rabbitmq and send test message to queue.

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
+	"github.com/streadway/amqp"
 	"net/http"
 )
 
@@ -13,6 +14,18 @@ func main() {
 	log.SetLevel(log.InfoLevel)
 	log.Info("running syncsets-api")
 	r := mux.NewRouter().StrictSlash(true)
+
+	conn, err := amqp.Dial("amqp://ffY5PQ_tMehsn2tryfCDvuEvVDIBoLYu:Sp7vDEG8J_E62XFi-6r3XWBQJJi0T1Sy@localhost:5672/")
+	if err != nil {
+		log.WithError(err).Fatal("error connecting to to rabbitmq")
+	}
+	defer conn.Close()
+	log.Info("rabbitmq connection established")
+	ch, err := conn.Channel()
+	if err != nil {
+		log.WithError(err).Fatal("error establishing rabbitmq channel")
+	}
+	defer ch.Close()
 
 	db, err := sql.Open("postgres", "user=postgres password=WYZVrmtdvuQlsq4hvo8C host=localhost dbname=syncsets sslmode=disable")
 	if err != nil {
@@ -22,6 +35,29 @@ func main() {
 	clusterHandler := handlers.NewClusterHandler(db)
 	r.HandleFunc("/clusters", clusterHandler.Get).Methods("GET")
 	r.HandleFunc("/clusters", clusterHandler.Post).Methods("POST")
+	q, err := ch.QueueDeclare(
+		"clusters", // name
+		false,      // durable
+		false,      // delete when unused
+		false,      // exclusive
+		false,      // no-wait
+		nil,        // arguments
+	)
+	if err != nil {
+		log.WithError(err).Fatal("error declaring rabbitmq queue")
+	}
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("hello world"),
+		})
+	if err != nil {
+		log.WithError(err).Error("error publishing test message")
+	}
 
 	syncsetHandler := handlers.NewSyncSetHandler(db)
 	r.HandleFunc("/syncsets", syncsetHandler.Get).Methods("GET")
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/lib/pq v1.8.0
 	github.com/sirupsen/logrus v1.7.0
+	github.com/streadway/amqp v1.0.0
 	k8s.io/api v0.19.4
 	k8s.io/apimachinery v0.19.4
 )
